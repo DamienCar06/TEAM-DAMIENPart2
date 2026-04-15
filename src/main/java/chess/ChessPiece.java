b0a3f3b8fef23b5dd1ad543bb1ddbcd4a54e4d28
DamienCar06 <da.cardenas06@gmail.com>
2026-04-15 16:39:54 -0500

    Feature: Update ChessPiece.java to include the features of the pieces of a chess game such as color and type AI: [Prompt] Asked for the features of piece in a chess game; [Adopt] piece type and piece color; [Change] ChessPiece.java

--- src/main/java/chess/ChessPiece.java
+++ src/main/java/chess/ChessPiece.java
@@ -1,5 +1,25 @@
 package chess;
 
 public class ChessPiece {
-    
+    final PieceType type;
+    final PieceColor color;
+
+    public ChessPiece(PieceType type, PieceColor color) {
+        this.type = type;
+        this.color = color;
+    }
+
+    public String getGlyph() {
+        if (color == PieceColor.WHITE) {
+            return type.whiteGlyph;
+        }
+        return type.blackGlyph;
+    }
+
+    public String getName() {
+        if (color == PieceColor.WHITE) {
+            return "White " + type.name();
+        } 
+        return "Black " + type.name();
+    }
 }
